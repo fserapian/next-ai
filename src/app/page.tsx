55626339d7d332eab88bc66c282fffb5c7272bcc
Fadi Serapian <fadi.serapian@gmail.com>
2024-09-24 19:59:15 -0400

    Add images in public and json data in data folder and render it as stringified json

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 // import Image from 'next/image';
+import stacks from '../data/stacks.json'
 
 export default function Home() {
     return (
         <div className="flex justify-center items-center h-full">
             <div className="flex justify-center items-center flex-col">
                 <div>What do you want to learn?</div>
-                <div>This is in the center</div>
+                <div>{JSON.stringify(stacks)}</div>
             </div>
         </div>
     );
