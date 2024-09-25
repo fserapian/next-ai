4e098297b194b1c70eddf58d228de1c35eb8a3d7
Fadi Serapian <fadi.serapian@gmail.com>
2024-09-24 20:50:14 -0400

    Update css for stack images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,19 +5,19 @@ import Link from 'next/link';
 export default function Home() {
     const renderStacks = () => {
         return (
-            <div className="flex space-x-2 justify-center items-center">
+            <div className="flex space-x-2 justify-center items-center mt-2">
                 {(Object.keys(stacks) as Array<keyof typeof stacks>).map(
                     (stackKey) => {
                         const stack = stacks[stackKey];
 
                         return (
-                            <Link href={stack.href} key={stack.href}>
-                                {/* <Image src={stack.logo} fill className='object-cover' alt="Logo" /> */}
+                            <Link href={stack.href} key={stack.href} className="border-2 border-solid rounded w-20 h-20 flex justify-center items-center p-3 hover:border-blue-400">
                                 <Image
                                     src={stack.logo}
                                     alt="Logo"
                                     width={75}
                                     height={75}
+                                    className="object-contain"
                                 />
                             </Link>
                         );
