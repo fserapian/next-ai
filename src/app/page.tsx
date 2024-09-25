cc8dbfe299cdce07693edab8c9a2b71e2512c285
Fadi Serapian <fadi.serapian@gmail.com>
2024-09-24 20:39:27 -0400

    Render stack images with links in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,37 @@
-// import Image from 'next/image';
-import stacks from '../data/stacks.json'
+import Image from 'next/image';
+import stacks from '../data/stacks.json';
+import Link from 'next/link';
 
 export default function Home() {
+    const renderStacks = () => {
+        return (
+            <div className="flex space-x-2 justify-center items-center">
+                {(Object.keys(stacks) as Array<keyof typeof stacks>).map(
+                    (stackKey) => {
+                        const stack = stacks[stackKey];
+
+                        return (
+                            <Link href={stack.href} key={stack.href}>
+                                {/* <Image src={stack.logo} fill className='object-cover' alt="Logo" /> */}
+                                <Image
+                                    src={stack.logo}
+                                    alt="Logo"
+                                    width={75}
+                                    height={75}
+                                />
+                            </Link>
+                        );
+                    }
+                )}
+            </div>
+        );
+    };
+
     return (
         <div className="flex justify-center items-center h-full">
             <div className="flex justify-center items-center flex-col">
                 <div>What do you want to learn?</div>
-                <div>{JSON.stringify(stacks)}</div>
+                <div>{renderStacks()}</div>
             </div>
         </div>
     );
