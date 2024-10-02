4906d2381c19f5cf53c4aafe9429f9562381c025
Fadi Serapian <fadi.serapian@gmail.com>
2024-10-01 20:49:43 -0400

    Add textarea prompt in stack page

--- src/app/learning/[stack]/page.tsx
+++ src/app/learning/[stack]/page.tsx
@@ -42,6 +42,14 @@ const LearningStackPage = ({ params }: { params: { stack: string } }) => {
                     </div>
                 </div>
             </div>
+
+            <div className="flex mb-8">
+                <textarea
+                    rows={4}
+                    placeholder="Enter your prompt here..."
+                    className="w-full p-2.5 text-sm text-gray-900 bg-slate-200 rounded-lg border border-gray-300"
+                />
+            </div>
         </div>
     );
 };
